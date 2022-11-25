49d683fdab8967a5377b6d59ade48e62a36ff38a
Fabian Lippold <fabianlippold1184@gmail.com>
2022-11-25 11:15:37 +0100

    Update Assignment 3.c

--- Lab 1/Assignment 3.c
+++ Lab 1/Assignment 3.c
@@ -1,24 +1,34 @@
 #include <inttypes.h>
+#include <stdio.h>
 
 #define LED_ADDRESS 0xFF200000
 #define KEY_ADDRESS 0xFF200050
 #define KEY_EC_ADDRESS 0xFF20005C
 
-uint8_t poll_keys(void) {
-    return 0b00001111 & *(uint8_t*) KEY_ADDRESS;
+uint32_t poll_keys(void) {
+    return 0b1111 & *(uint32_t*) KEY_ADDRESS;
 }
 
-void write_leds(uint8_t leds) {
-    *(uint8_t*) LED_ADDRESS = leds;
+void write_leds(uint32_t leds) {
+    *(uint32_t*) LED_ADDRESS = leds;
 }
 
-int32_t main(void) {
-    uint8_t state = 0;
+int main(void) {
+    uint32_t last_keys = 0;
+
     while (1) {
-        uint8_t new_state = state ^ poll_keys();
-        if (new_state != state) {
-            state = new_state;
-            write_leds(state);
-        }
+    	uint32_t new_keys = poll_keys();
+
+    	if ((last_keys & 0b1) == 0 && (new_keys & 0b1) == 1) {
+    		write_leds(0b1);
+    	} else if(((last_keys >> 1) & 0b1) == 0 && ((new_keys >> 1) & 0b1) == 1) {
+    		write_leds(0b10);
+    	} else if(((last_keys >> 2) & 0b1) == 0 && ((new_keys >> 2) & 0b1) == 1) {
+    		write_leds(0b100);
+    	} else if(((last_keys >> 3) & 0b1) == 0 && ((new_keys >> 3) & 0b1) == 1) {
+    		write_leds(0b1000);
+    	}
+
+    	last_keys = new_keys;
     }
 }
